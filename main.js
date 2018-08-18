cda8d7d4c674bb93acf0deb5563df297b2add2bf
cranberriez <jakevilevac@gmail.com>
2018-08-18 02:02:35 -0700

    Aug18Upd

--- main.js
+++ main.js
@@ -9,21 +9,20 @@ function navScroll() {
 }
 
 $(function(){
-
   console.log("Stupid google errors below, sry")
   // Add smooth scrolling to all links (Not Made By Jacob)
-  $("a").on('click', function(event) {
+  $("a").on('click', function(e) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
-      event.preventDefault();
+      e.preventDefault();
       // Store hash
       var hash = this.hash;
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 500, function(){
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
@@ -34,4 +33,13 @@ $(function(){
    navScroll()
 	});
   navScroll()
+})
+
+
+var i = 0;
+$('article').each(function(){
+  i++;
+  var head = $(this).find('h1').text().replace(/Rules/g,'');;
+  $(this).attr('id', head.split(/[^a-zA-Z]/)[0] )
+  $('.gs-toc ul:nth-child(1)').append(`<li class='head'><a href='#${head.split(/[^a-zA-Z]/)[0]}'>${head}</a></li>`)
 })
